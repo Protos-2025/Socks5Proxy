e88080c4ce64340a36511941735faab746d383e1
MaximoWehncke <mwehncke@itba.edu.ar>
2025-12-02 20:10:58 -0300

    feat(server): [WIP] pam's auth handlers

--- src/server/include/pam.h
+++ src/server/include/pam.h
@@ -6,21 +6,30 @@
 #include <sys/socket.h>
 #include "../../shared/include/selector.h"
 #include "../../shared/include/buffer.h"
+#include "../include/pamAuth.h"
 #include "stm.h"
 
+#define PAM_ATTACHMENT(key) ( (struct pam *)(key)->data)
 
 enum pamela_state {
-  AUTH,
-  // add more
-};
+	AUTH,
+	// add more
 
+
+	PAM_DONE,
+    PAM_ERROR
+};
 
 struct pam {
   int client_fd;
   struct sockaddr_storage client_addr;
   socklen_t client_addr_len;
-    buffer client_buffer;
-    uint8_t client_buffer_data[BUFFER_SIZE];
+  buffer client_buffer;
+  uint8_t client_buffer_data[BUFFER_SIZE];
+  union {
+    struct pamAuth_st auth;
+    // add more
+  } client;
 
 
   struct state_machine stm;

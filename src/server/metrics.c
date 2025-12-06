0e7c6f0fa730656a9cbb2b87fd9999d8c4df77dc
MaximoWehncke <mwehncke@itba.edu.ar>
2025-12-06 11:53:55 -0300

    feat(server): add metric function calls
    not adding calls to:
    registerBytesTransferred because we first need the copy state
    getMetricSnapshot because we are still developing pam's client

--- src/server/metrics.c
+++ src/server/metrics.c
@@ -3,7 +3,7 @@
 static struct metricSnapshot metrics;
 
 
-void metricsInit() {
+void metrics_init() {
     metrics.currentConnections = 0;
     metrics.totalConnections = 0;
     metrics.totalBytesSent = 0;
@@ -11,26 +11,26 @@ void metricsInit() {
 }
 
 
-void registerNewConnection() {
+void register_new_connection() {
     metrics.currentConnections++;
     metrics.totalConnections++;
 }
 
 
-void registerConnectionClosed() {
+void register_connection_closed() {
     if (metrics.currentConnections > 0) {
         metrics.currentConnections--;
     }
 }
 
 
-void registerBytesTransferred(int bytesReceived, int bytesSent) {
-    metrics.totalBytesReceived += bytesReceived;
-    metrics.totalBytesSent += bytesSent;
+void register_bytes_transferred(int bytes_received, int bytes_sent) {
+    metrics.totalBytesReceived += bytes_received;
+    metrics.totalBytesSent += bytes_sent;
 }
 
 
-void getMetricsSnapshot(struct metricSnapshot *snapshot) {
+void get_metrics_snapshot(struct metricSnapshot *snapshot) {
     snapshot->currentConnections = metrics.currentConnections;
     snapshot->totalConnections = metrics.totalConnections;
     snapshot->totalBytesSent = metrics.totalBytesSent;

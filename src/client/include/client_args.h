fb272e0764a131640f39fc73010306f6005ef290
Lchiossone2004 <lchiossone@itba.edu.ar>
2025-12-09 18:39:31 -0300

    feat(client): new client files added to main

---
+++ src/client/include/client_args.h
@@ -0,0 +1,18 @@
+#ifndef CLIENT_ARGS_H
+#define CLIENT_ARGS_H
+
+struct ClientArgs {
+    const char* host;
+    unsigned short port;
+    const char* option;
+
+    // arguments following the option
+    const char* arg1;
+    const char* arg2;
+    const char* arg3;
+};
+
+int parse_client_args(int argc, char* argv[], struct ClientArgs* out);
+void print_help(const char* prog);
+
+#endif

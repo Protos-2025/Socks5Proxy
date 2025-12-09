de113267053abdcf894b567fccb05de88286c76d
Tomás Pietravallo <tpietravallo@itba.edu.ar>
2025-12-09 19:27:52 -0300

    fix(copy): shutdown correctly

--- src/server/states/copy.c
+++ src/server/states/copy.c
@@ -10,18 +10,37 @@
 #define IS_CLIENT_DATA(connection, key) (connection->client_fd == key->fd)
 
 static int update_target_interests(FdSelector s, CopySt * target) {
-	FdInterest ret = OP_NOOP;
-    if ((target->interests & OP_READ) && buffer_can_write(target->buffer)) {
-        ret |= OP_WRITE;
-    }
-	if (((target->interests & OP_WRITE)) && buffer_can_write(target->buffer)) {
+    FdInterest ret = OP_NOOP; /* interests for target->fd */
+    FdInterest oet = OP_NOOP; /* interests for target->otherCopySt->fd */
+    CopySt *other = target->otherCopySt;
+
+    // target can read if target's buffer has space to write
+    if (buffer_can_write(other->buffer)) {
 		ret |= OP_READ;
 	}
-	if (SELECTOR_SUCCESS != selector_set_interest(s, target->fd, ret)) {
+
+    // target can write if target's buffer has data to read
+    if (buffer_can_read(target->buffer)) {
+		ret |= OP_WRITE;
+	}
+
+    // other can read if other's buffer has space to write
+    if (buffer_can_write(target->buffer)) {
+		oet |= OP_READ;
+	}
+
+    // other can write if other's buffer has data to read
+    if (buffer_can_read(other->buffer)) {
+		oet |= OP_WRITE;
+	}
+
+	if ((SELECTOR_SUCCESS != selector_set_interest(s, target->fd, ret)) || (SELECTOR_SUCCESS != selector_set_interest(s, other->fd, oet))) {
         LOG_ERROR("Failed to update interests for fd=%d", target->fd);
 		return -1;
 	};
+
     target->interests = ret;
+    other->interests = oet;
 	return ret;
 }
 
@@ -62,21 +81,27 @@ unsigned socksv5_copy_read(struct selector_key * key) {
 
 	ssize_t readBytes = recv(from->fd, writePtr, canWrite, 0);
 
+    LOG_TRACE("%s interests before read from %s: OP_READ=%s, OP_WRITE=%s", IS_CLIENT_DATA(connection, key) ? "origin" : "client", IS_CLIENT_DATA(connection, key) ? "client" : "origin", (to->interests & OP_READ) ? "true" : "false", (to->interests & OP_WRITE) ? "true" : "false");
+	LOG_TRACE("%s interests before read from %s: OP_READ=%s, OP_WRITE=%s", IS_CLIENT_DATA(connection, key) ? "client" : "origin", IS_CLIENT_DATA(connection, key) ? "client" : "origin", (from->interests & OP_READ) ? "true" : "false", (from->interests & OP_WRITE) ? "true" : "false");
+
     if (readBytes > 0) {
         LOG_TRACE("Read %zd bytes from %s... writing to %s buffer (fd=%d into fd=%d): '%.*s'", readBytes, IS_CLIENT_DATA(connection, key) ? "client" : "origin", IS_CLIENT_DATA(connection, key) ? "origin" : "client", from->fd, to->fd, (int)readBytes, writePtr);
         buffer_write_adv(to->buffer, readBytes);
-        if (update_target_interests(key->s, to) < 0 || update_target_interests(key->s, from) < 0) {
+        if (update_target_interests(key->s, from) < 0) {
             LOG_ERROR("Failed to update interests after read");
             return ERROR;
         };
     } else {
+        LOG_TRACE("Couldn't read from %s (fd=%d)", IS_CLIENT_DATA(connection, key) ? "client" : "origin", from->fd);
         if (readBytes < 0) {
             LOG_WARN("Read error (%d) from fd=%d: %s", errno, from->fd, strerror(errno));
         }
 		shutdown(from->fd, SHUT_RD);
         from->interests &= ~OP_READ;
+        selector_set_interest(key->s, from->fd, from->interests);
         shutdown(to->fd, SHUT_WR);
         to->interests &= ~OP_WRITE;
+        selector_set_interest(key->s, to->fd, to->interests);
 	}
 
     LOG_TRACE("%s interests after read from %s: OP_READ=%s, OP_WRITE=%s", IS_CLIENT_DATA(connection, key) ? "origin" : "client", IS_CLIENT_DATA(connection, key) ? "client" : "origin", (to->interests & OP_READ) ? "true" : "false", (to->interests & OP_WRITE) ? "true" : "false");
@@ -100,21 +125,27 @@ unsigned socksv5_copy_write(struct selector_key * key) {
 
     ssize_t writtenBytes = send(toFd, readPtr, canRead, 0);
 
+    LOG_TRACE("%s interests before write to %s: OP_READ=%s, OP_WRITE=%s", IS_CLIENT_DATA(connection, key) ? "origin" : "client", IS_CLIENT_DATA(connection, key) ? "client" : "origin", (from->otherCopySt->interests & OP_READ) ? "true" : "false", (from->otherCopySt->interests & OP_WRITE) ? "true" : "false");
+    LOG_TRACE("%s interests before write to %s: OP_READ=%s, OP_WRITE=%s", IS_CLIENT_DATA(connection, key) ? "client" : "origin", IS_CLIENT_DATA(connection, key) ? "client" : "origin", (from->interests & OP_READ) ? "true" : "false", (from->interests & OP_WRITE) ? "true" : "false");
+
     if (writtenBytes > 0) {
         LOG_TRACE("Wrote %zd bytes to %s (fd=%d) from %s (fd=%d) - '%.*s'", writtenBytes, IS_CLIENT_DATA(connection, key) ? "client" : "origin", toFd, IS_CLIENT_DATA(connection, key) ? "origin" : "client", from->fd, (int)writtenBytes, readPtr);
         buffer_read_adv(from->buffer, writtenBytes);
-        if (update_target_interests(key->s, from) < 0 || update_target_interests(key->s, from->otherCopySt) < 0) {
+        if (update_target_interests(key->s, from) < 0) {
             LOG_ERROR("Failed to update interests after write");
             return ERROR;
         };
     } else {
+        LOG_TRACE("Couldn't write to %s (fd=%d)", IS_CLIENT_DATA(connection, key) ? "client" : "origin", toFd);
         if (writtenBytes < 0) {
             LOG_WARN("Write error (%d) to fd=%d: %s", errno, toFd, strerror(errno));
         }
         shutdown(toFd, SHUT_WR);
-        from->otherCopySt->interests &= ~OP_WRITE;
-        shutdown(from->fd, SHUT_RD);
-        from->interests &= ~OP_READ;
+        from->interests &= ~OP_WRITE;
+        selector_set_interest(key->s, from->fd, from->interests);
+        shutdown(from->otherCopySt->fd, SHUT_RD);
+        from->otherCopySt->interests &= ~OP_READ;
+        selector_set_interest(key->s, from->otherCopySt->fd, from->otherCopySt->interests);
     }
 
     LOG_TRACE("%s interests after write to %s: OP_READ=%s, OP_WRITE=%s", IS_CLIENT_DATA(connection, key) ? "origin" : "client", IS_CLIENT_DATA(connection, key) ? "client" : "origin", (from->otherCopySt->interests & OP_READ) ? "true" : "false", (from->otherCopySt->interests & OP_WRITE) ? "true" : "false");

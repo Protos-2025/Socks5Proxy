145a67c3756316139c9207f5cd9713a7f7ad81ad
Lucia Oliveto <luciamoliveto@gmail.com>
2025-12-10 10:05:17 -0300

    refactor(pam): interpret pam addr and port args to create socket

--- src/server/server.c
+++ src/server/server.c
@@ -18,11 +18,10 @@
 #include "logger.h"
 #include "args.h"
 
-#define GET_BUFFER_IDX(i, n) (((i) + (n)) == BUFFER_SIZE ? 0 : ((i) + (n)))
-#define GET_BUFFER_MAX_IDX_TO_READ(w) ((w) == 0 ? BUFFER_SIZE : (w))
+#define GET_SOCK_DOMAIN(l) ((l) == sizeof(struct sockaddr_in) ? (((struct sockaddr_in *)&addr)->sin_family) : (((struct sockaddr_in6 *)&addr)->sin6_family))
 
 static void signal_handler(const int signal);
-static int interpret_socket_args(struct socks5args args, struct sockaddr_storage * addr);
+static int interpret_socket_args(struct sockaddr_storage * addr_storage, char * addr, unsigned short port);
 
 typedef struct custom_key {
     uint8_t * buffer;
@@ -31,15 +30,14 @@ typedef struct custom_key {
     struct custom_key * other_party;
 } CustomKey;
 
-static int server;
-static int pamServer;
 static bool done = false;
 
-int main(const int argc, const char **argv) {
-	logger_init();
+int main(const int argc, const char ** argv) {
+    int server, pam_server;
+
+    logger_init();
     metrics_init();
     users_init();
-	unsigned port;
 
     struct socks5args args;
     parse_args(argc, (char **) argv, &args);
@@ -53,18 +51,16 @@ int main(const int argc, const char **argv) {
 
     // <---------------------------- create proxy server socket ---------------------------->
     struct sockaddr_storage addr;
-	int addrlen = 0, domain;
+	int addrlen = 0;
 
-	if ((addrlen = interpret_socket_args(args, &addr)) < 0) {
+	if ((addrlen = interpret_socket_args(&addr, args.socks_addr, args.socks_port)) < 0) {
         errorMsg = "interpreting socket arguments";
         goto finally;
     }
 
 	LOG_DEBUG("Starting server...");
 
-    domain = (addrlen == sizeof(struct sockaddr_in) ? ((struct sockaddr_in *)&addr)->sin_family : ((struct sockaddr_in6 *)&addr)->sin6_family);
-
-    if ((server = socket(domain, SOCK_STREAM, 0)) < 0) {
+    if ((server = socket(GET_SOCK_DOMAIN(addrlen), SOCK_STREAM, 0)) < 0) {
         errorMsg = "unable to create socket";
         goto finally;
     }
@@ -83,33 +79,37 @@ int main(const int argc, const char **argv) {
 
     LOG_INFO("Proxy server listening on addr %s:%d (TCP)", args.socks_addr, args.socks_port);
 
-	// <---------------------------- create pam server socket ---------------------------->
-	unsigned pamPort = 4242; /* TODO: grab it from an argument*/
-	LOG_DEBUG("Starting pam server", pamPort);
-	struct sockaddr_in pamAddr;
-	memset(&pamAddr, 0, sizeof(pamAddr));
-	pamAddr.sin_family      = AF_INET;
-	pamAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	pamAddr.sin_port        = htons(pamPort);
 
-	if ((pamServer = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	// <---------------------------- create pam server socket ---------------------------->
+    struct sockaddr_storage pam_addr;
+	int pam_addrlen = 0, pam_domain;
+    
+    if ((pam_addrlen = interpret_socket_args(&pam_addr, args.mng_addr, args.mng_port)) < 0) {
+        errorMsg = "interpreting pam socket arguments";
+        goto finally;
+    }
+    
+    LOG_DEBUG("Starting pam server...");
+
+	if ((pam_server = socket(GET_SOCK_DOMAIN(pam_addrlen), SOCK_STREAM, 0)) < 0) {
 		errorMsg = "unable to create pam socket";
 		goto finally;
 	}
 
-	setsockopt(pamServer, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+	setsockopt(pam_server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
-	if (bind(pamServer, (struct sockaddr *) &pamAddr, sizeof(pamAddr)) < 0) {
+	if (bind(pam_server, (struct sockaddr *) &pam_addr, pam_addrlen) < 0) {
 		errorMsg = "unable to bind pam socket";
 		goto finally;
 	}
 
-	if (listen(pamServer, MAX_PENDING_CONNECTIONS) < 0) {
+	if (listen(pam_server, MAX_PENDING_CONNECTIONS) < 0) {
 		errorMsg = "unable to listen on pam server";
 		goto finally;
 	}
 
-	LOG_INFO("Pam server listening on TCP port %d", pamPort);
+	LOG_INFO("Pam server listening on addr %s:%d (TCP)", args.mng_addr, args.mng_port);
+
 
 	// <----------------------------------- setup signals ----------------------------------->
     signal(SIGTERM, signal_handler);
@@ -117,13 +117,14 @@ int main(const int argc, const char **argv) {
 	signal(SIGQUIT, signal_handler);
     signal(SIGABRT, signal_handler);
 
+
     // <--------------------------------- configure selector --------------------------------->
     if (selector_fd_set_nio(server) == -1) {
         errorMsg = "getting server socket flags";
         goto finally;
     }
 
-    if (selector_fd_set_nio(pamServer) == -1) {
+    if (selector_fd_set_nio(pam_server) == -1) {
         errorMsg = "getting pam server socket flags";
         goto finally;
     }
@@ -171,12 +172,13 @@ int main(const int argc, const char **argv) {
         goto finally;
     }
 
-    ss = selector_register(selector, pamServer, &pam, OP_READ, NULL);
+    ss = selector_register(selector, pam_server, &pam, OP_READ, NULL);
 
     if (ss != SELECTOR_SUCCESS) {
         errorMsg = "registering pam fd";
         goto finally;
     }
+
 
     // <------------------------------------ execute server ------------------------------------>
     while (!done) {
@@ -221,29 +223,29 @@ static void signal_handler(const int signal) {
     done = true;
 }
 
-static int interpret_socket_args(struct socks5args args, struct sockaddr_storage * addr) {
-    int ipv6 = strchr(args.socks_addr, ':') != NULL;
+static int interpret_socket_args(struct sockaddr_storage * addr_storage, char * addr, unsigned short port) {
+    int ipv6 = strchr(addr, ':') != NULL;
 
     if (ipv6) {
-        struct sockaddr_in6 * socks6 = (struct sockaddr_in6 *) addr;
+        struct sockaddr_in6 * socks6 = (struct sockaddr_in6 *) addr_storage;
         memset(socks6, 0, sizeof(struct sockaddr_in6));
 		socks6->sin6_family = AF_INET6;
         socks6->sin6_addr = in6addr_any;
-        socks6->sin6_port = htons(args.socks_port);
-        if (inet_pton(AF_INET6, args.socks_addr, &socks6->sin6_addr) != 1) {
-            LOG_FATAL("Invalid IPv6 address: %s", args.socks_addr);
+        socks6->sin6_port = htons(port);
+        if (inet_pton(AF_INET6, addr, &socks6->sin6_addr) != 1) {
+            LOG_FATAL("Invalid IPv6 address: %s", addr);
 			return -1;
 		}
 		return sizeof(struct sockaddr_in6);
 	}
 
-    struct sockaddr_in * socks4 = (struct sockaddr_in *) addr;
+    struct sockaddr_in * socks4 = (struct sockaddr_in *) addr_storage;
     memset(socks4, 0, sizeof(struct sockaddr_in));
     socks4->sin_family = AF_INET;
     socks4->sin_addr.s_addr = INADDR_ANY;
-    socks4->sin_port = htons(args.socks_port);
-    if (inet_pton(AF_INET, args.socks_addr, &socks4->sin_addr) != 1) {
-        LOG_FATAL("Invalid IPv4 address: %s", args.socks_addr);
+    socks4->sin_port = htons(port);
+    if (inet_pton(AF_INET, addr, &socks4->sin_addr) != 1) {
+        LOG_FATAL("Invalid IPv4 address: %s", addr);
         return -1;
     }
 

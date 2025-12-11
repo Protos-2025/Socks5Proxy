916fe80d12c2a4c6c717709e0224947655644e8a
MaximoWehncke <mwehncke@itba.edu.ar>
2025-12-11 15:52:57 -0300

    Revert "feat(pamserver): allow logger level set & get"
    This reverts commit 08b24c075eed3746fcd6b90cc8f517450552ec87.

--- src/server/logger.c
+++ src/server/logger.c
@@ -236,12 +236,3 @@ void free_logger() {
 	free_queue(logQueue);
 	logQueue = NULL;
 }
-
-int logger_get_min_level() {
-    return minLogLevel;
-}
-
-void logger_set_min_level(int level) {
-    minLogLevel = level;
-}
-
